a03e016ed536e9f3790c1cde1d64b899979c7f11
hang58hao <30553127+hang58hao@users.noreply.github.com>
2018-01-17 17:03:51 +0800

    cdc

--- downLoadProject/ViewController.m
+++ downLoadProject/ViewController.m
@@ -48,6 +48,10 @@
     NSLog(@"这是合伙人的测试");
 }
 
+-(void)hahah{
+    NSLog(@"这是主干的更新");
+}
+
 -(void)downloadRate:(float)rate withDownloadUrl:(NSString *)downloadUrl{
     _downLoadProgress = rate;
     _progressLabel.text = [NSString stringWithFormat:@"%.1f%%",rate];
